62b1dd4fd0cec79c942d1d31c34f1f68ec3e7827
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-01-18 03:14:18 +0900

    Almost made it
    Work I have done
    
    1. JTable:
    - Cell contents are now cannot be changed
    - Column length difference
    - Files that fail to edit are marked in red
    
    2. Remove Button
    - Remove selected files
    
    Functions to be added
    - Function to delete files by DEL key

--- MusicTagEditor/src/MusicTag/test.java
+++ MusicTagEditor/src/MusicTag/test.java
@@ -1,24 +1,26 @@
 package MusicTag;
 
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.LinkedList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
@@ -26,19 +28,24 @@ import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 
 import net.iharder.dnd.FileDrop;
-import javax.swing.JLabel;
-import javax.swing.JList;
 
 public class test extends JPanel {
 	
 	//private JFrame frame;
+	private static JFrame win = new JFrame();
+
 
 	private JTextField PathInput = new JTextField("");
 	private JTable DataSet = new JTable();
+	private DefaultTableModel model = new DefaultTableModel (new String[]{"File Name", "Artist", "Title"},0){
+		public boolean isCellEditable(int row, int column) { //all cells false
+			return false;
+		}	
+	};
+	
 	JScrollPane MusicList = new JScrollPane(DataSet);
 	
-	private DefaultTableModel model = new DefaultTableModel(new String[]{"File Name", "Artist", "Title"},0);
-	//contentPane.add(scrollPane);
+	private static int Xsize = 800;
 	
 	/**
 	 * Music list
@@ -51,19 +58,26 @@ public class test extends JPanel {
 	LinkedList<String> pathlist = new LinkedList<String>();
 	private static int index = 0; 
 	
+	
 	/**
 	 * Create the application.
 	 */
 	public test() {
+		setLayout(null);
 		//setLayout(null);
 		//String colNames[] = {"파일이름", "Artist", "Title"};
 		//DefaultTableModel model =  new DefaultTableModel(colNames, 0);
-		
-		
-		//MusicList = new JTable(model);
+
 		DataSet.setModel(model);
+		int width[]= {10,3,5};
+		for(int i=0; i<3;i++) {
+			TableColumn col = DataSet.getColumnModel().getColumn(i);
+			col.setPreferredWidth(Xsize/18*width[i]);
+		}
+		
 		MusicList = new JScrollPane(DataSet);
 		add(MusicList);
+			
 		initialize();
 	}
 	
@@ -89,12 +103,12 @@ public class test extends JPanel {
 	
 	public static void main(String[] args) {
 		test panel = new test();
-		JFrame win = new JFrame();
-		win.add(panel);
-		win.setBounds(550, 100, 510, 610);
+		win.getContentPane().add(panel);
+		win.setBounds(500, 150, Xsize, 600);
 		win.setTitle("Music Tag Editor");
 		//win.setSize(540,400);
 		win.setVisible(true);
+		
 	}
 	
 
@@ -107,18 +121,37 @@ public class test extends JPanel {
 		PathInput.setFont(font2);
 		PathInput.setText("Enter directory address here");
 	}
+	 
 	
-	public void phrase2() { // Path request phrases
-		Font font2 = new Font("맑은 고딕", Font.ITALIC, 12);
-		DataSet.setForeground(Color.gray);
-		DataSet.setFont(font2);
-		//MusicList.setText("Drag and drop files here");
-	}
+	
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		
+
+		
+		
+		class key implements KeyListener{  
+            public void keyPressed(KeyEvent e) {
+            	if( e.getKeyCode() == 127 ) {
+            		System.out.println( "aaaaa" );
+            	
+            		removeMusic();
+            	}
+                System.out.println( e );
+            }
+
+            public void keyReleased(KeyEvent e){ }          
+            public void keyTyped(KeyEvent e) { }            
+                        
+        }       
+        
+		win.addKeyListener(new key());
+		win.setFocusable(true);
+		
 		/*
 		 * about Main Window (JFrame)
 		 */
@@ -135,8 +168,13 @@ public class test extends JPanel {
 		/*
 		 * about PathInputWindow (textField)
 		 */
-		PathInput.setBounds(32, 26, 427, 21);
-		//frame.getContentPane().
+		
+		JLabel lblNewLabel = new JLabel("Music File List");
+		lblNewLabel.setBounds(17, 52, 110, 15);
+		add(lblNewLabel);
+		
+		
+		PathInput.setBounds(17, 13, Xsize-50, 23);
 		add(PathInput);
 		PathInput.setColumns(10);
 		phrase1();
@@ -171,25 +209,34 @@ public class test extends JPanel {
 		/*
 		 * about MusicListWindow (textArea)
 		 */
-		MusicList.setBounds(33, 80, 426, 390);
-		//frame.getContentPane().
+		MusicList.setBounds(17, 68, Xsize-50, 439);
 		add(MusicList);
-		phrase2();
+
 		
-	
 		new FileDrop(MusicList, /* dragBorder, */ new FileDrop.Listener() {
 			public void filesDropped(java.io.File[] fileList) {
-				findMusicFile(fileList);
-				//DefaultTableModel model = (DefaultTableModel)DataSet.getModel();
-				
+				findMusicFile(fileList);				
 			} // end filesDropped
 		}); // end FileDrop.Listener
-
 		//MusicList.setEditable (false); //make text area uneditalbe
 		
 		
 		
 
+		/*
+		 * about Remove Button
+		 */
+		JButton RemoveButton = new JButton("Remove");
+		RemoveButton.setFont(new Font("굴림", Font.BOLD, 14));
+		RemoveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removeMusic();
+			}
+		});
+		RemoveButton.setBounds(421, 520, 100, 30);
+		add(RemoveButton);
+		
+		
 		/*
 		 * about Clear Button
 		 */
@@ -198,11 +245,9 @@ public class test extends JPanel {
 		ClearButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				clearList();
-				phrase2();
 			}
 		});
-		ClearButton.setBounds(195, 500, 120, 40);
-		//frame.getContentPane().
+		ClearButton.setBounds(544, 520, 100, 30);
 		add(ClearButton);
 		
 		
@@ -216,31 +261,43 @@ public class test extends JPanel {
 				RunTagEditor();
 			}
 		});
-		RunningButton.setBounds(339, 500, 120, 40);
+		RunningButton.setBounds(667, 520, 100, 30);
 		//frame.getContentPane().
 		add(RunningButton);
-		
-		JLabel lblNewLabel = new JLabel("Music File List");
-		lblNewLabel.setBounds(32, 61, 92, 15);
-		//frame.getContentPane().
-		add(lblNewLabel);
 	}
 	
 	public void findMusicFile(File[] fileList) {
 		if (DataSet.getForeground() != Color.black) {
-		//	MusicList.setText(""); 
+			clearList();
 			DataSet.setForeground(Color.black);
-			Font font1 = new Font("맑은 고딕", Font.PLAIN, 12);
-			DataSet.setFont(font1);
 		}
-		for( int i = 0; i < fileList.length; i++ ) { //fileName = file.getName(); try
+		for( int i = 0; i < fileList.length; i++ ) { 
 			String fileName = fileList[i].getName();
 			if (fileName.matches(".*.mp3")) {						  
 				try{						
 					index++;						
 					pathlist.add(fileList[i].getCanonicalPath());							
-					namelist.add(fileName);				
-					model.addRow(new String[] {fileName,"",""});
+					namelist.add(fileName);		
+					
+					File file = new File(pathlist.get(i)); // MP3 파일의 경로를 이용하여 File Object를 생성한다.
+					try {
+						// make modification to the tag class
+						AudioFile f = AudioFileIO.read(file);
+
+						Tag tag = f.getTag();
+						
+						String artist2 = tag.getFirst(FieldKey.ARTIST);
+						String title2 = tag.getFirst(FieldKey.TITLE);
+						
+						model.addRow(new String[] {fileName,artist2,title2});
+						// and then commit the change
+						// f.commit(); //you can use this instead of code under
+						AudioFileIO.write(f);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					
 				//	DataSet.append(fileName + "\n"); //show up file name					
 				}						
 				catch( java.io.IOException e ) { } 					
@@ -249,39 +306,49 @@ public class test extends JPanel {
 		
 	}
 	
+	public void removeMusic() {
+		int[] row = DataSet.getSelectedRows();
+		for(int i=0; i<row.length;i++) {
+			model.removeRow(row[i]-i);//subtract i to account for index difference from the rows which have already been removed 
+			namelist.remove(row[i]-i);
+			pathlist.remove(row[i]-i);
+		}
+		index -= row.length;
+	}
+	
 	public void clearList() {
 		index = 0;
 		namelist.clear();
 		pathlist.clear();
+		model.setNumRows(0);
 	}
 	
 	public void RunTagEditor() {		
-	//	MusicList.setText("");
 		String fileName, artist, title; // = "";
 		int idx = 0; // index of " - "
 		int idx2 = 0; // index of ".mp3", 
 		int cnt = 0, fail = 0; // count of file(s) successfully modified
 
 		for(int i=0 ; i<index;i++) {
-			fileName = namelist.get(i);
+			fileName = namelist.get(fail);
 			if (fileName.matches(".* - .*")) {
 				idx = fileName.indexOf(" - ");
 				idx2 = fileName.indexOf(".mp3");
 				artist = fileName.substring(0, idx);
 				title = fileName.substring(idx + 3, idx2);
-				rewriteTag(pathlist.get(i), artist, title);
+				rewriteTag(pathlist.get(fail), artist, title);				
 				cnt++;
+
+				model.removeRow(fail);
+				namelist.remove(fail);
+				pathlist.remove(fail);
 			} else {
 				DataSet.setForeground(Color.red);
-		//		if (DataSet.getText().isBlank())
-		//			DataSet.append(" ERROR! There is no \" - \" in:\n");
 				fail++;
-		//		DataSet.append(fail + ". " + fileName + "\n");
 			}
 		}
-//		if (DataSet.getText().isBlank())
-			phrase2();
-		clearList();
+		
+		index = index - cnt;
 		JOptionPane.showMessageDialog(null,cnt+" file(s) successfully modified\n"+fail+
 				" file(s) failed to modified","Information",JOptionPane.WARNING_MESSAGE);
 	}
