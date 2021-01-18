a49140f8cdd7e5d351d128603220619bb4920f97
DovyIsFree <63338253+DovyIsFree@users.noreply.github.com>
2021-01-18 17:22:14 +0900

    Duplicate prevention
    prevent duplication of files by searching linkedlist

--- MusicTagEditor/src/MusicTag/test.java
+++ MusicTagEditor/src/MusicTag/test.java
@@ -9,6 +9,8 @@ import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 import javax.swing.JButton;
@@ -30,93 +32,61 @@ import org.jaudiotagger.tag.Tag;
 import net.iharder.dnd.FileDrop;
 
 public class test extends JPanel {
-	
-	//private JFrame frame;
-	private static JFrame win = new JFrame();
 
+	// private JFrame frame;
+	private static JFrame win = new JFrame();
 
 	private JTextField PathInput = new JTextField("");
 	private JTable DataSet = new JTable();
-	private DefaultTableModel model = new DefaultTableModel (new String[]{"File Name", "Artist", "Title"},0){
-		public boolean isCellEditable(int row, int column) { //all cells false
+	private DefaultTableModel model = new DefaultTableModel(new String[] { "File Name", "Artist", "Title" }, 0) {
+		public boolean isCellEditable(int row, int column) { // all cells false
 			return false;
-		}	
+		}
 	};
-	
+
 	JScrollPane MusicList = new JScrollPane(DataSet);
-	
+
 	private static int Xsize = 800;
-	
+
 	/**
-	 * Music list
-	 * namelist will be shown on TextArea
-	 * pahtlist will be used to edit mp3 files
-	 * two lists will have same index for same mp3 files
-	 * and it will make easy to add and delete files from list
+	 * Music list namelist will be shown on TextArea pahtlist will be used to edit
+	 * mp3 files two lists will have same index for same mp3 files and it will make
+	 * easy to add and delete files from list
 	 */
 	LinkedList<String> namelist = new LinkedList<String>();
 	LinkedList<String> pathlist = new LinkedList<String>();
-	private static int index = 0; 
-	
-	
+	private static int index = 0;
+
 	/**
 	 * Create the application.
 	 */
 	public test() {
 		setLayout(null);
-		//setLayout(null);
-		//String colNames[] = {"파일이름", "Artist", "Title"};
-		//DefaultTableModel model =  new DefaultTableModel(colNames, 0);
 
 		DataSet.setModel(model);
-		int width[]= {10,3,5};
-		for(int i=0; i<3;i++) {
+		int width[] = { 10, 3, 5 };
+		for (int i = 0; i < 3; i++) {
 			TableColumn col = DataSet.getColumnModel().getColumn(i);
-			col.setPreferredWidth(Xsize/18*width[i]);
+			col.setPreferredWidth(Xsize / 18 * width[i]);
 		}
-		
+
 		MusicList = new JScrollPane(DataSet);
 		add(MusicList);
-			
+
 		initialize();
 	}
-	
-	
+
 	
 	/**
 	 * Launch the application.
 	 */
-	
-	/*
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					test window = new test();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
-	
 	public static void main(String[] args) {
 		test panel = new test();
 		win.getContentPane().add(panel);
-		win.setBounds(500, 150, Xsize, 600);
+		win.setBounds(300, 150, Xsize, 600);
 		win.setTitle("Music Tag Editor");
-		//win.setSize(540,400);
 		win.setVisible(true);
-		
-		
-	
-		
 	}
-	
-
-	
-
 
 	public void phrase1() { // Path request phrases
 		Font font2 = new Font("맑은 고딕", Font.ITALIC, 12);
@@ -124,59 +94,31 @@ public class test extends JPanel {
 		PathInput.setFont(font2);
 		PathInput.setText("Enter directory address here");
 	}
-	 
-	
-	
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
-		
-		class key implements KeyListener{  
-            public void keyPressed(KeyEvent e) {
-            	if( e.getKeyCode() == 127 ) {
-            		System.out.println( "aaaaa" );
-            	
-            		removeMusic();
-            	}
-                System.out.println( e );
-            }
 
-            public void keyReleased(KeyEvent e){ }          
-            public void keyTyped(KeyEvent e) { }            
-                        
-        }       
-        
-		DataSet.addKeyListener(new key());
-		DataSet.setFocusable(true);
-		
-		
-	
 		/*
 		 * about Main Window (JFrame)
 		 */
-		
+
 		/*
-		frame = new JFrame();
-		frame.setBounds(550, 100, 510, 610);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //what is this for???
-		frame.getContentPane().setLayout(null);
-		frame.setTitle("Music Tag Editor");
-		*/
-		
-		
+		 * frame = new JFrame(); frame.setBounds(550, 100, 510, 610);
+		 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //what is this for???
+		 * frame.getContentPane().setLayout(null); frame.setTitle("Music Tag Editor");
+		 */
+
 		/*
 		 * about PathInputWindow (textField)
 		 */
-		
+
 		JLabel lblNewLabel = new JLabel("Music File List");
 		lblNewLabel.setBounds(17, 52, 110, 15);
 		add(lblNewLabel);
-		
-		
-		PathInput.setBounds(17, 13, Xsize-50, 23);
+
+		PathInput.setBounds(17, 13, Xsize - 50, 23);
 		add(PathInput);
 		PathInput.setColumns(10);
 		phrase1();
@@ -190,6 +132,7 @@ public class test extends JPanel {
 					PathInput.setText("");
 				}
 			}
+
 			public void focusLost(FocusEvent e) {
 				if (PathInput.getText().isBlank())
 					phrase1();
@@ -197,31 +140,44 @@ public class test extends JPanel {
 		});
 
 		PathInput.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { //Press Enter key
+			public void actionPerformed(ActionEvent e) { // Press Enter key
 				File fileList[] = new File(PathInput.getText()).listFiles();
 				findMusicFile(fileList);
 				phrase1();
 			}
 		});
-		
 
-		
 		/*
 		 * about MusicListWindow (textArea)
 		 */
-		MusicList.setBounds(17, 68, Xsize-50, 439);
+		MusicList.setBounds(17, 68, Xsize - 50, 439);
 		add(MusicList);
 
-		
 		new FileDrop(MusicList, /* dragBorder, */ new FileDrop.Listener() {
 			public void filesDropped(java.io.File[] fileList) {
-				findMusicFile(fileList);				
+				findMusicFile(fileList);
 			} // end filesDropped
 		}); // end FileDrop.Listener
-		//MusicList.setEditable (false); //make text area uneditalbe
-		
-		
-		
+		// MusicList.setEditable (false); //make text area uneditalbe
+		class key implements KeyListener {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == 127) {
+					System.out.println("aaaaa");
+
+					removeMusic();
+				}
+				System.out.println(e);
+			}
+
+			public void keyReleased(KeyEvent e) {
+			}
+
+			public void keyTyped(KeyEvent e) {
+			}
+
+		}
+		DataSet.addKeyListener(new key());
+		DataSet.setFocusable(true);
 
 		/*
 		 * about Remove Button
@@ -235,8 +191,7 @@ public class test extends JPanel {
 		});
 		RemoveButton.setBounds(421, 520, 100, 30);
 		add(RemoveButton);
-		
-		
+
 		/*
 		 * about Clear Button
 		 */
@@ -249,8 +204,7 @@ public class test extends JPanel {
 		});
 		ClearButton.setBounds(544, 520, 100, 30);
 		add(ClearButton);
-		
-		
+
 		/*
 		 * about Running Button
 		 */
@@ -262,81 +216,74 @@ public class test extends JPanel {
 			}
 		});
 		RunningButton.setBounds(667, 520, 100, 30);
-		//frame.getContentPane().
+		// frame.getContentPane().
 		add(RunningButton);
 	}
-	
+
 	public void findMusicFile(File[] fileList) {
 		if (DataSet.getForeground() != Color.black) {
 			clearList();
 			DataSet.setForeground(Color.black);
 		}
-		for( int i = 0; i < fileList.length; i++ ) { 
-			String fileName = fileList[i].getName();
-			if (fileName.matches(".*.mp3")) {						  
-				try{						
-					index++;						
-					pathlist.add(fileList[i].getCanonicalPath());							
-					namelist.add(fileName);		
-					
+		for (int i = 0; i < fileList.length; i++) {
+			String path = fileList[i].getPath();
+			int idx = pathlist.indexOf(path); //return -1 if there is no path in pathlist
+
+			if (idx == -1) { 
+				String fileName = fileList[i].getName();
+
+				if (fileName.matches(".*.mp3")) { //add only mp3 files
+					index++;
+					pathlist.add(fileList[i].getPath());
+					namelist.add(fileName);
+
 					File file = new File(pathlist.get(i)); // MP3 파일의 경로를 이용하여 File Object를 생성한다.
 					try {
-						// make modification to the tag class
-						AudioFile f = AudioFileIO.read(file);
-
-						Tag tag = f.getTag();
-						
+						Tag tag = AudioFileIO.read(file).getTag();
 						String artist2 = tag.getFirst(FieldKey.ARTIST);
 						String title2 = tag.getFirst(FieldKey.TITLE);
-						
-						model.addRow(new String[] {fileName,artist2,title2});
-						// and then commit the change
-						// f.commit(); //you can use this instead of code under
-						AudioFileIO.write(f);
 
+						model.addRow(new String[] { fileName, artist2, title2 });
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
-					
-				//	DataSet.append(fileName + "\n"); //show up file name					
-				}						
-				catch( java.io.IOException e ) { } 					
-			} // end for: through each dropped file				
+				}
+			}
 		}
-		
 	}
-	
+
 	public void removeMusic() {
 		int[] row = DataSet.getSelectedRows();
-		for(int i=0; i<row.length;i++) {
-			model.removeRow(row[i]-i);//subtract i to account for index difference from the rows which have already been removed 
-			namelist.remove(row[i]-i);
-			pathlist.remove(row[i]-i);
+		for (int i = 0; i < row.length; i++) {
+			model.removeRow(row[i] - i);// subtract i to account for index difference from the rows which have already
+										// been removed
+			namelist.remove(row[i] - i);
+			pathlist.remove(row[i] - i);
 		}
 		index -= row.length;
 	}
-	
+
 	public void clearList() {
 		index = 0;
 		namelist.clear();
 		pathlist.clear();
 		model.setNumRows(0);
 	}
-	
-	public void RunTagEditor() {		
+
+	public void RunTagEditor() {
 		String fileName, artist, title; // = "";
 		int idx = 0; // index of " - "
-		int idx2 = 0; // index of ".mp3", 
+		int idx2 = 0; // index of ".mp3",
 		int cnt = 0, fail = 0; // count of file(s) successfully modified
 
-		for(int i=0 ; i<index;i++) {
+		for (int i = 0; i < index; i++) {
 			fileName = namelist.get(fail);
 			if (fileName.matches(".* - .*")) {
 				idx = fileName.indexOf(" - ");
 				idx2 = fileName.indexOf(".mp3");
 				artist = fileName.substring(0, idx);
 				title = fileName.substring(idx + 3, idx2);
-				rewriteTag(pathlist.get(fail), artist, title);				
+				rewriteTag(pathlist.get(fail), artist, title);
 				cnt++;
 
 				model.removeRow(fail);
@@ -347,10 +294,11 @@ public class test extends JPanel {
 				fail++;
 			}
 		}
-		
+
 		index = index - cnt;
-		JOptionPane.showMessageDialog(null,cnt+" file(s) successfully modified\n"+fail+
-				" file(s) failed to modified","Information",JOptionPane.WARNING_MESSAGE);
+		JOptionPane.showMessageDialog(null,
+				cnt + " file(s) successfully modified\n" + fail + " file(s) failed to modified", "Information",
+				JOptionPane.WARNING_MESSAGE);
 	}
 
 	public static void rewriteTag(String path, String artist, String title) {
